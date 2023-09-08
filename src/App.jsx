e3f45b841d1484a5ee883a766cc226a11ddb34b3
Christian Fernandez <christian14@gmail.com>
2023-09-08 14:58:07 -0300

    Una simple comaracion de las cantidas de lineas que le se lleva el manejo de estado y los hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
-import MiComponente from "./MiComponente";
+import { Component, useState } from "react";
+// import MiComponente from "./MiComponente";
 
+// Aca veremos una simple comparacion de entre useState y estado manejado con clases.
+class App extends Component{
+ state  = {
+   contador : 0 
+  }
+  incrementar = () => {
+    this.setContador({ contador: this.state.contador + 1})
+  }
+  render() {
+    return (
+      <div>
+      Contador: {this.state.contador}
+      <div>
+        <button onClick={this.incrementar}>Incrementar</button>
+        </div>
+    </div>
 
-const App = () => {
-  return (
-    <MiComponente miProp = {'Hola mundo'}/>
-  )
+    )
+  }
 }
+
+
+
+// const App = () => {
+//   const [contador, setContador] = useState(0)
+//   return (
+//     <div>
+//       Contador: {contador}
+//       <div><button onClick={()=> setContador(contador + 1)}>Incrementar 1</button></div>
+//     </div>
+//   )
+  
+// }
 
 export default App
